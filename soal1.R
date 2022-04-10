7c738775c3cdda1b3f847bc0d910bf23844690e7
sillentboy <89970736+sillentboy@users.noreply.github.com>
2022-04-10 21:31:48 +0700

    Update soal1.R

--- soal1.R
+++ soal1.R
@@ -1,48 +1,38 @@
 #soal no 1
 
 # a. Peluang penyurvei bertemu orang yang tidak menghadiri acara vaksinasi
-# ketika diketikahui x=3 dan p=0.20
-p = 0.20
-n = 3
-dgeom(x = n, prob = p)
+# ketika diketikahui x=3 dan p=0.2
+p = 0.2
+x = 3
+
+Peluang <- dgeom(x, p)
+Peluang
 
 # b. Mencari mean dari 10000 data random, prob = 0.20, dan X=3
-mean(rgeom(n = 10000, prob = p) == 3)
+n = 10000
+p = 0.20
+m <- mean(rgeom(n, p) == 3)
+m
 
 # c. Perbandingan a dan b
-# pada soal 1.a itu merupakan nilai exact nya, yaitu 0.1024
-# sedangkan 1.b merupakan nilai dari simulasi ketika diambil dengan 10000 data random pada kasus yang serupa
-# dan ternyata hasil nya mendekati nilai exact nya
+# Poin a akan menghasilkan nilai peluang yang tetap karena distribusi geometriknya sama
+# Poin b menghasilkan nilai yang berbeda karena distribusi geometik bernilai acak
 
 # d. Membuat histogram distribusi geometrik dengan X=3 gagal sebelum sukses pertama
-library(dplyr)
-library(ggplot2)
-
-data.frame(x = 0:10, prob = dgeom(x = 0:10, prob = p)) %>%
-  mutate(Failures = ifelse(x == n, n, "other")) %>%
-  ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
-  geom_col() +
-  geom_text(
-    aes(label = round(prob,2), y = prob + 0.01),
-    position = position_dodge(0.9),
-    size = 3,
-    vjust = 0
-  ) +
-  labs(title = "Probability of X = 3 Failures Prior to First Success",
-       subtitle = "Geometric(.2)",
-       x = "Failures prior to first success (x)",
-       y = "Probability")
-
+set.seed(0)
+hist(rgeom(n, prob = p),
+     main="Histogram Geometrik",
+     xlim = c(0,25),
+     xlab="X",
+     col="green",
+)
 
 # e. Mencari nilai rataan dan varians dari distribusi geometric
-p=0.2
+p = 0.2
+n = 3
 
-#mean
-mean1 = 1 / p
-mean1
+rataan = n/p
+rataan
 
-#variance
-var1 = (1 - p) / p^2
-var1
-
-
+varians = (n-p)/(p^2)
+varians
